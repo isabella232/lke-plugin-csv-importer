d5663ba6779bdddc45f0415adf92a9af82274a31
aminerhanemi <amine@linkurio.us>
2021-08-09 16:53:36 +0100

    Develop (#22)
    * fix(error): fix row error number (#19)
    Co-authored-by: Renato Ramos G <renato@linkurio.us>

--- src/backend/graphItemService.ts
+++ src/backend/graphItemService.ts
@@ -236,7 +236,8 @@ export class GraphItemService {
     }
     GraphItemService.checkNonEmptyHeaders(headers);
     return {
-      total: count,
+      // The header is not an item
+      total: count - 1,
       headers: headers,
       batchedRows: batchedRows,
       badRows: [[RowErrorMessage.TOO_MANY_OR_MISSING_PROPERTIES, tooManyOrMissingProperties]]
@@ -308,7 +309,8 @@ export class GraphItemService {
     }
     GraphItemService.checkNonEmptyHeaders(headers);
     return {
-      total: count,
+      // The header is not an item
+      total: count - 1,
       headers: headers,
       batchedRows: batchedRows,
       badRows: [
